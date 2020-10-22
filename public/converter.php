fe6294371096bcbe2d9d502cb5951f9c4e71b1e3
AMIR479 <69789786+AMIR479@users.noreply.github.com>
2020-10-22 09:31:59 +0200

    maj

---
+++ public/converter.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Converter</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+   
+    <link rel="stylesheet" href="login.php">
+    <title> Converter</title>
+</head>
+<body class="bg-secundary">
+    <header >
+        <div class="container text-center text-success  ">
+            <h1  style="background-color: beige;"><strong>Projet Converter</strong> </h1>
+        </div> <br> <br>     
+    </header>
+    <div>
+
+           <div class="text-center">
+            <form action="">
+                <h2  class="text-center text-white bg-success">Converter Page</h2>
+                <label for="">Valeur en C°</label>
+                <input type="text" placeholder="Entrer Entrer la valeur en degré C°"> <br>
+                
+                <button type="button" class="btn btn-success ">Valider</button> <span><button type="button" class="btn btn-dark ">Reset</button></span> 
+            </form>
+        </div>
+        
+    
+   
+   
+   
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</body>
+</html>
+</body>
+</html>
---
+++ public/login.php
@@ -0,0 +1,35 @@
+<?php
+<link rel="stylesheet" href="login.php">
+    <title> Converter</title>
+</head>
+<body class="bg-secundary">
+    <header >
+        <div class="container text-center text-success  ">
+            <h1  style="background-color: beige;"><strong>Projet Converter</strong> </h1>
+        </div> <br> <br>     
+    </header>
+    <div>
+
+           <div class="text-center">
+            <form action="">
+                <h2  class="text-center text-white bg-success">LOGIN FORM</h2>
+                <label for="">Identifiant</label>
+                <input type="text" placeholder="Entrer votre identifiant"> <br>
+                <label for="">Mot de passe</label>
+                <input type="text" placeholder="Entrer votre mot de passe"> <br>
+
+                <button type="button" class="btn btn-success ">Valider</button> <span><button type="button" class="btn btn-dark ">Reset</button></span> 
+            </form>
+        </div>
+        z
+    
+   
+   
+   
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</body>
+</html>
+?>
